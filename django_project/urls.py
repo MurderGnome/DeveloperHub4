6b3469223b4e6794ea75e1ba36771b10161a8cde
MurderGnome <132377074+MurderGnome@users.noreply.github.com>
2023-10-25 17:22:52 +0000

    sdfsdf
    sdf

--- django_project/urls.py
+++ django_project/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from user_profiles.views import login_view  # Import the login_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('user_profiles/', include('user_profiles.urls')),  # Including the app's urls here
+    path('', login_view, name='home'),  # Direct root URL to login_view
 ]
+
+
